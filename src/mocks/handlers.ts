93b294796641bccfa2b456332e575cf307cbb059
규나다 <qnada0118@gmail.com>
2026-06-26 18:08:03 +0900

    [MOG-22] MSW handler 구조화

--- src/mocks/handlers.ts
+++
@@ -1,6 +0,0 @@
-import { http, HttpResponse } from 'msw';
-import { fixtures } from './fixtures';
-
-export const handlers = [
-  http.get('/api/users', () => HttpResponse.json(fixtures.users)),
-];
