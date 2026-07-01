7ac19a179677511cf52db959df484012a38e6e30
규나다 <qnada0118@gmail.com>
2026-07-01 13:14:30 +0900

    [MOG-41] 약속 기록 상세 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ExampleRoutes from '@/routes/ExampleRoutes';
 import HomePage from '@/pages/Home/HomePage';
 import LoginPage from '@/pages/Login/LoginPage';
+import PromiseDetailPage from '@/pages/PromiseDetail/PromiseDetailPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/promise-detail" element={<PromiseDetailPage />} />
         <Route path="/example/*" element={<ExampleRoutes />} />
       </Routes>
     </BrowserRouter>
