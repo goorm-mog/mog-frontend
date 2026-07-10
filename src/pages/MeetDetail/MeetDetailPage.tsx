a624b18a00d4547529f05c2fbc34c79ddd3b8a8d
규나다 <qnada0118@gmail.com>
2026-07-10 15:08:23 +0900

    [MOG-75] 뒷배경 잘리는 이슈

--- src/pages/MeetDetail/MeetDetailPage.tsx
+++ src/pages/MeetDetail/MeetDetailPage.tsx
@@ -57,13 +57,13 @@ function MeetDetailPage() {
       <TopAppBar
         title="기록"
         showBack
-        className="fixed top-0 left-1/2 z-50 w-full max-w-[390px] -translate-x-1/2"
+        className="fixed top-0 left-1/2 z-50 w-full max-w-[430px] -translate-x-1/2"
         rightSlot={<span aria-hidden className="block size-4" />}
         onBack={() => navigate('/home')}
       />
 
-      <main className="fixed top-[50px] bottom-0 left-1/2 w-full max-w-[390px] -translate-x-1/2 overflow-hidden bg-background">
-        <section className="h-full overflow-y-auto pb-[19px]">
+      <main className="fixed top-[50px] bottom-0 left-1/2 w-full max-w-[430px] -translate-x-1/2 overflow-hidden bg-background">
+        <section className="mx-auto h-full w-full max-w-[390px] overflow-y-auto pb-[19px]">
           <StepHeader
             showStep={false}
             wrapperClassName="px-[14px] pt-[19px] pb-0"
